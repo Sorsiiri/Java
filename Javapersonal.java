54c5ad697309abe242665f51b1450997c6b0cfb5
Sorsiiri <robin.nyholm08@gmail.com>
2024-09-26 13:06:38 +0300

    Add files via upload

---
+++ Javapersonal.java
@@ -0,0 +1,20 @@
+
+public class Javapersonal {
+
+    public static void main(String args[]) {
+        String name = "Robin";
+        String LastName = "Nyholm";
+        System.out.println(name + " " + LastName);
+        System.out.print(name);
+        System.out.print(LastName);
+
+        int age = 16;
+        System.out.println(age);
+        
+        double height = 182;
+        System.out.println(height);
+        
+        boolean student = true;
+        System.out.println(student);
+    }
+}
